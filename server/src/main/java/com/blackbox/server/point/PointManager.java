25b5f489c0e2e8365ae737a39e01647fd3c67581
Andrew Wright <ayax79@gmail.com>
2010-02-22 10:06:18 -0800

    point manager complaining about a prev commit i made.

--- server/src/main/java/com/blackbox/server/point/PointManager.java
+++ server/src/main/java/com/blackbox/server/point/PointManager.java
@@ -7,6 +7,7 @@ package com.blackbox.server.point;
 
 import com.blackbox.foundation.point.IPointManager;
 import com.blackbox.foundation.point.Point;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
@@ -15,7 +16,7 @@ import java.util.Collection;
  *
  *
  */
-// - disabled @Service("pointManager")
+@Service("pointManager")
 public class PointManager implements IPointManager {
 
     // - disabled @Resource
